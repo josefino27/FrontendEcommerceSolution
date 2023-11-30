63a40fac194dbedb6481f5e6f43da4679fdd01d3
josefino27 <jodanu19@gmail.com>
2023-11-29 21:17:56 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Home } from "./components/Home";
+import { Footer } from "./components/layout/Footer";
+import { Header } from "./components/layout/Header";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
