f3c87e0dabb1dd022bd24e4fdee26c533db12060
alfanet-soporte <soporte.archivar@gmail.com>
2023-12-09 17:34:53 -0500

    filtro de rating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import ProductDetail from "./components/product/ProductDetail";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getCategories } from "./actions/categoryAction";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories({}));
+  }, [dispatch])
   return (
     <Router>
       <div className="App">
         <Header />
 
         <div className="container container-fluid">
-            <Routes>
-              <Route path="/"  element={<Home />}  />
-              <Route path="/product/:id" element={<ProductDetail />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+          </Routes>
         </div>
-        
-        
+
         <Footer />
       </div>
     </Router>
