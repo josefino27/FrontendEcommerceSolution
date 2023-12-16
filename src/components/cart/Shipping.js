4c4675e76d1bb578f5dd3e5541b7797f837b1a95
alfanet-soporte <soporte.archivar@gmail.com>
2023-12-16 16:38:57 -0500

    check out ok

--- src/components/cart/Shipping.js
+++ src/components/cart/Shipping.js
@@ -1,65 +1,61 @@
 import React, { Fragment, useEffect, useState } from "react";
-import MetaData from "../layout/MetaData";
+import MetaData from '../layout/MetaData';
 import { useAlert } from "react-alert";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { resetUpdateStatus } from "../../slices/securitySlice";
+import {resetUpdateStatus} from '../../slices/securitySlice';
 import { saveAddressInfo } from "../../actions/cartAction";
 
 const Shipping = () => {
-  const alert = useAlert();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { direccionEnvio, isUpdated, errores } = useSelector(
-    (state) => state.security
-  );
-  const { countries } = useSelector((state) => state.country);
+    const alert = useAlert();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { direccionEnvio, isUpdated, errores } = useSelector(state => state.security);
+    console.log(direccionEnvio);
+    const {countries} = useSelector(state => state.country);
 
-  const { direccion, setDireccion } = useState(
-    direccionEnvio ? direccionEnvio.direccion : ""
-  );
-  const { ciudad, setCiudad } = useState(
-    direccionEnvio ? direccionEnvio.ciudad : ""
-  );
-  const { departamento, setDepartamento } = useState(
-    direccionEnvio ? direccionEnvio.departamento : ""
-  );
-  const { codigoPostal, setCodigoPostal } = useState(
-    direccionEnvio ? direccionEnvio.codigoPostal : ""
-  );
-  const { pais, setPais } = useState(
-    direccionEnvio ? direccionEnvio.pais : "US"
-  );
+    const [direccion, setDireccion] = useState( direccionEnvio ? direccionEnvio.direccion : "" );
+    const [ciudad, setCiudad] = useState( direccionEnvio ? direccionEnvio.ciudad : "" );
+    const [departamento, setDepartamento] = useState( direccionEnvio ? direccionEnvio.departamento : "" );
+    const [codigoPostal, setCodigoPostal] = useState( direccionEnvio ? direccionEnvio.codigoPostal : "" );
+    const [pais, setPais] = useState( direccionEnvio ? direccionEnvio.pais : "US" );
 
-  useEffect(() => {
-    if (isUpdated) {
-      //navigate("/order/confirm");
-      dispatch(resetUpdateStatus({}));
-      alert.success("Se almaceno la direccion de envio");
+    useEffect( () => {
+        if(isUpdated)
+        {
+            //navege hacia la siguiente pasarela de pago - order confirm
+            // navigate("/order/confirm");
+            dispatch(resetUpdateStatus({}))
+            alert.success("Se almaceno la direccion de envio");
+        }
+
+        if(errores)
+        {
+            errores.map(error => alert.error(error));
+        }
+
+
+    }, [dispatch, errores, alert, isUpdated]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const request = {
+            direccion, 
+            ciudad, 
+            departamento,
+            codigoPostal,
+            pais
+        };
+
+
+        dispatch(saveAddressInfo(request));
     }
 
-    if (errores) {
-      errores.map((error) => alert.error(error));
-    }
-  }, [dispatch, errores, alert, isUpdated]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    const request = {
-      direccion,
-      ciudad,
-      departamento,
-      codigoPostal,
-      pais,
-    };
-
-    dispatch(saveAddressInfo(request));
-  };
 
   return (
     <Fragment>
-      <MetaData titulo={"Shipping"} />
+      <MetaData title={"Informacion de Envio"}/>
       <div className="row wrapper">
         <div className="col-10 col-lg-5">
           <form className="shadow-lg" onSubmit={submitHandler}>
@@ -70,8 +66,8 @@ const Shipping = () => {
                 type="text"
                 id="address_field"
                 className="form-control"
-                value={direccion ?? ""}
-                onChange={(e) => setDireccion(e.target.value)}
+                value={direccion ?? ''}
+                onChange={e => setDireccion(e.target.value)}
                 required
               />
             </div>
@@ -82,20 +78,21 @@ const Shipping = () => {
                 type="text"
                 id="ciudad_field"
                 className="form-control"
-                value={ciudad ?? ""}
-                onChange={(e) => setCiudad(e.target.value)}
+                value={ciudad ?? ''}
+                onChange={e => setCiudad(e.target.value)}
                 required
               />
             </div>
 
+           
             <div className="form-group">
               <label htmlFor="departamento_field">Departamento</label>
               <input
                 type="text"
                 id="departamento_field"
                 className="form-control"
-                value={departamento ?? ""}
-                onChange={(e) => setDepartamento(e.target.value)}
+                value={departamento ?? ''}
+                onChange={e => setDepartamento(e.target.value)}
                 required
               />
             </div>
@@ -106,28 +103,38 @@ const Shipping = () => {
                 type="text"
                 id="codigoPostal_field"
                 className="form-control"
-                value={codigoPostal ?? ""}
-                onChange={(e) => setCodigoPostal(e.target.value)}
+                value={codigoPostal ?? ''}
+                onChange={e => setCodigoPostal(e.target.value)}
                 required
               />
             </div>
+
+
+
 
             <div className="form-group">
               <label htmlFor="country_field">Pais</label>
               <select
                 id="country_field"
                 className="form-control"
-                value={pais ?? "US"}
-                onChange={(e) => setPais(e.target.value)}
+                value={pais ?? 'US'}
+                onChange={e => setPais(e.target.value)}
                 required
               >
-                {countries.map((country) => (
-                  <option key={country.id} value={country.name}>
-                    {country.name}
-                  </option>
-                ))}
+                {
+                    countries.map(country => (
+                        <option key={country.id} value={country.name}>
+                            {country.name}
+                        </option>
+                    ))
+
+                }
+
+
               </select>
             </div>
+
+            
 
             <button
               id="shipping_btn"
